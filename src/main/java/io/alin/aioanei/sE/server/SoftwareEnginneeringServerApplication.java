9ec4c6f68efe4726af7eb5b9386a90c9dfca9fd1
Alin Aioanei <AlinA@yonder.local>
2018-07-04 17:22:20 +0300

    added project on git

---
+++ src/main/java/io/alin/aioanei/sE/server/SoftwareEnginneeringServerApplication.java
@@ -0,0 +1,12 @@
+package io.alin.aioanei.sE.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareEnginneeringServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareEnginneeringServerApplication.class, args);
+	}
+}
---
+++ src/test/java/io/alin/aioanei/sE/server/SoftwareEnginneeringServerApplicationTests.java
@@ -0,0 +1,16 @@
+package io.alin.aioanei.sE.server;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SoftwareEnginneeringServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
